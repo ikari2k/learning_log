135f20f10b99c7c74a4c10b42e33cf5cdb28f8fa
ikari2k <pluta.m@gmail.com>
2024-03-07 15:20:09 +0100

    added entry model

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -10,3 +10,18 @@ class Topic(models.Model):
     def __str__(self):
         """Return model representation in form of string"""
         return self.text
+
+
+class Entry(models.Model):
+    """Concrete info o learning progress"""
+
+    topic = models.ForeignKey(Topic, on_delete=models.CASCADE)
+    text = models.TextField()
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "entries"
+
+    def __str__(self):
+        """Return model representation in form of string"""
+        return f"{self.text[:50]}..."
