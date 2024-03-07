48e94a78ebeb98fe99030fa72efcd9521b43109c
ikari2k <pluta.m@gmail.com>
2024-03-07 19:57:41 +0100

    Adding new entry functionality added - missing view

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import redirect, render
 
 from .models import Topic
-from .forms import TopicForm
+from .forms import TopicForm, EntryForm
 
 
 def index(request):
@@ -40,3 +40,25 @@ def new_topic(request):
     # Display empty form
     context = {"form": form}
     return render(request, "learning_logs/new_topic.html", context)
+
+
+def new_entry(request, topic_id):
+    """Add new entry to given topic"""
+    topic = Topic.objects.get(id=topic_id)
+
+    if request.method != "POST":
+        # No data was passed, creating new form
+        form = EntryForm()
+    else:
+        # Data sent by POST, let's process them
+        form = EntryForm(data=request.POST)
+        if form.is_valid():
+            new_entry = form.save(commit=False)
+            new_entry.topic = topic
+            new_entry.save()
+            # Return to topics page after successful save
+            return redirect("learning_logs:topic", topic_id=topic_id)
+
+    # Display empty form
+    context = {"topic": topic, "form": form}
+    return render(request, "learning_logs/new_entry.html", context)
